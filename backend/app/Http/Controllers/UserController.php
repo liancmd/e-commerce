29278d21fec8eaa2cf8a2b332a20609fc4866c93
liancmd <lianabdiansyahtanjung@gmail.com>
2021-09-19 23:05:19 +0700

    berhasil membuat fungsi logout

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -3,10 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     function register(request $request) {
-        return $request->input();
+        $user = new User;
+        $user->nama = $request->input("nama");
+        $user->email = $request->input("email");
+        $user->password = Hash::make($request->input("password"));
+        $user->save();
+        return $user;
+        // return $request->input();
     }
 }
